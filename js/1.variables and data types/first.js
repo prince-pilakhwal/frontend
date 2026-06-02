57aa9d6cd1c349b5a1969cb48f3e6c163af5ca90
prince-pilakhwal <pilakhwalprince@gmail.com>
2026-06-02 19:15:26 +0530

    .git folder added

---
+++ js/1.variables and data types/first.js
@@ -0,0 +1,90 @@
+console.log("Welcome to Hello world");
+console.log("Prince");
+console.log("Welcome to js");
+/*iska output dekhne keliiye 
+inspect ke console section me jaake
+each line enter krke aata h
+output*/
+
+name="MSD";
+console.log("name");
+console.log(name);
+/*double quotes hatade if u 
+wanna print a int or string
+likewise*/
+price=150.50;
+console.log(price);
+isFollow=false;
+console.log(isFollow);
+
+
+Console="apna college";
+console.log(Console);
+
+/* console="apna college";
+console.log(console);
+
+wont work*/
+let fullName="Prince";
+/*let fullName="Harssh";
+this will error in console
+because redeclaring it
+
+but u can update it like this */
+
+fullName="Reverse_Silence";
+
+
+console.log(fullName);
+
+
+var name="YuH";
+var name="yuh";
+var name="yuH";
+
+console.log(name);
+/*in var u can redeclare it too
+as well as update, so its
+bad so never use em always use 
+let and for const use const*/
+
+const PI=3.14;
+console.log(PI);
+
+/*const variables me neither can u
+redeclare and nor update em*/
+
+
+let a;
+console.log(a);
+/*a is a undefined variable*/
+
+/*const b;
+console.log(b);
+
+this will give a error because
+constants need to be given
+a value let is okay 
+const is not to leave undefined*/
+
+
+
+/* let is a block scope variable
+*/
+
+{
+    let c=6;
+    console.log(c);
+
+
+
+}
+{
+
+    let c =7;
+    console.log(c);
+}
+/*you can also see the datatype by using 
+typeof (nameofvariable) in the console ,
+pls do it to see it urself*/
+
